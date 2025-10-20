1ca67df577bd811eacb03f2bdfd667c101005abf
Nadja <nadja.luckhaupt@gmail.com>
2025-10-20 14:15:05 +0200

    sign up action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,12 @@
 "use server";
 
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import prisma from "@/db/prisma";
 import { signIn, signOut } from "@/auth";
 
 import { signInFormSchema } from "../validators";
+import { signUpFormSchema } from "../validators";
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -29,4 +32,39 @@ export async function signInWithCredentials(
 
 export async function signOutUser() {
   await signOut();
+}
+
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    const plainPassword = user.password;
+    const hashedPassword = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: hashedPassword,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "Sign up successful." };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: "User could not be signed up." };
+  }
 }
