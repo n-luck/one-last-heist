c95b33f0d878125b5df4e69ee2fa4779db48f602
Nadja <nadja.luckhaupt@gmail.com>
2025-10-29 14:35:06 +0100

    styling update: buttons, colors

--- app/loading.tsx
+++ app/loading.tsx
@@ -10,7 +10,7 @@ const LoadingPage = () => {
       }}
     >
       <div className="flex justify-center">
-        <div className="w-16 h-16 border-4 border-teal-600 border-t-transparent rounded-full animate-spin"></div>
+        <div className="w-16 h-16 md:w-36 md:h-36 border-4 border-cyan-600 border-t-transparent rounded-full animate-spin"></div>
       </div>
     </div>
   );
