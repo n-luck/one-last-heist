b6e1534ff67d05b6b73a2bb9e47bc31b7dc90d87
Nadja <nadja.luckhaupt@gmail.com>
2026-01-12 14:00:10 +0100

    reset password design

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -2,6 +2,16 @@
 
 import { useSearchParams, useRouter } from "next/navigation";
 import { resetPassword } from "@/lib/actions/user.actions";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function ResetPasswordPage() {
   const params = useSearchParams();
@@ -21,14 +31,35 @@ export default function ResetPasswordPage() {
   if (!token) return <p>Invalid link</p>;
 
   return (
-    <form action={onSubmit}>
-      <input
-        type="password"
-        name="password"
-        placeholder="New password"
-        required
-      />
-      <button type="submit">Reset password</button>
-    </form>
+    <section className="w-full max-w-md mx-auto p-4">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center h1-bold mb-1">
+            Reset Password
+          </CardTitle>
+          <CardDescription className="text-center">
+            Enter your information below to change your password.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="gap-4">
+          <form action={onSubmit}>
+            <div className="space-y-6">
+              <div className="space-y-2">
+                <Label htmlFor="name">New Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="New password"
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Reset password
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </section>
   );
 }
